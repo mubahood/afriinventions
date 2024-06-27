18e3e6b51fc79c6eeb4fc46528cf981d8b48f219
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-06-28 00:32:16 +0300

    refactor: Update JwtMiddleware to return the next middleware in case of an exception

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -77,7 +77,7 @@ class JwtMiddleware extends BaseMiddleware
             } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json(['status' => 'Token is Expired']);
             } else {
-                return Api_Utils::error($e->getMessage());
+                return $next($request); 
             }
         }
         return $next($request);
