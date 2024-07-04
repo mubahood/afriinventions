12e7acb64c320fbca29c5c86623cd80d7ce1b4b0
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-07-05 02:56:24 +0300

    chore: Refactor file upload logic in Utils.php

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -39,9 +39,9 @@ class Utils extends Model
         }
     }
 
- 
-   
- 
+
+
+
     public static function sendNotification(
         $msg,
         $receiver,
@@ -484,8 +484,8 @@ class Utils extends Model
         //check if $r does not contain public and add it
         if (!(str_contains($r, 'public'))) {
             $r = $r . "/public";
-        } 
-        
+        }
+
         return $r;
     }
 
@@ -506,11 +506,18 @@ class Utils extends Model
                 isset($file['error']) &&
                 isset($file['size'])
             ) {
-                $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+                /*  $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
                 $file_name = time() . "-" . rand(100000, 1000000) . "." . $ext;
                 $destination = Utils::docs_root() . '/storage/images/' . $file_name;
 
-                $res = move_uploaded_file($file['tmp_name'], $destination);
+                $res = move_uploaded_file($file['tmp_name'], $destination); */
+
+                //get file extension
+                $file_extension = $file->getClientOriginalExtension();
+                $file_name = time() . "_" . rand(100000000, 1000000000) . "." . $file_extension;
+                $public_path = public_path() . "/storage/images";
+                $res = $file->move($public_path, $file_name);
+                $url = 'images/' . $file_name;
                 if (!$res) {
                     continue;
                 }
