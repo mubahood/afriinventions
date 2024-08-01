4fadc8756b6dc453f5fb6afda36b70f6f1f50861
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-08-02 01:22:40 +0300

    a

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/test', function () {
+    //get last order by id
+    $order = \App\Models\Order::latest()->first();
+    //send_mail_to_admin
+    \App\Models\Order::send_mail_to_admin($order);
+    die("test done");
+});
+
 Route::match(['get', 'post'], '/pay', function () {
     $id = 1;
     if (isset($_GET['id'])) {
